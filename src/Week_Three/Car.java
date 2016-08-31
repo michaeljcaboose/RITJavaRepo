7184df5fea41b407612e7cf4f4b079cb594e90f1
Matthew Panebianco <mpane1@u.brockport.edu>
2016-08-31 19:19:01 -0400

    Create shells of required classes

---
+++ src/Week_Three/Car.java
@@ -0,0 +1,7 @@
+package Week_Three;
+
+/**
+ * Created by Panebianco on 8/31/16.
+ */
+public class Car {
+}
---
+++ src/Week_Three/Orders.java
@@ -0,0 +1,15 @@
+package Week_Three;
+
+/**
+ * Created by Panebianco on 8/31/16.
+ *
+ * Collects user input to create vehicle objects for
+ * order submission
+ *
+ */
+public class Orders {
+
+    public static void main(String [] args){
+        System.exit(0);
+    }
+}
